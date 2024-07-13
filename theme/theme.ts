a43b00ffaeeadfd2e74a26af620b54c42dd9578f
Harsh Dodiya <harsh.dodiya@achyutlabs.com>
2024-07-14 04:11:17 +0530

    global styles in theme

--- theme/theme.ts
+++ theme/theme.ts
@@ -7,6 +7,35 @@ import { Menu } from "./menu";
 // https://github.com/chakra-ui/chakra-ui/blob/v2/packages/theme/src/utils/is-chakra-theme.ts
 
 export const theme = extendTheme({
+  styles: {
+    global: {
+      html: {
+        width: "100%",
+        height: "100%",
+        margin: "0px",
+        padding: "0px",
+        "@media screen and (max-width: 800px)": {
+          overflowX: "hidden",
+        },
+      },
+      body: {
+        overflowX: "hidden",
+      },
+      "&::-webkit-scrollbar": {
+        "background-color": "#F5F5F5",
+        width: "4px",
+      },
+      "&::-webkit-scrollbar-track": {
+        width: "6px",
+        "-webkit-box-shadow": "inset 0 0 6px rgba(0,0,0,0.3)",
+        "background-color": "#F5F5F5",
+      },
+      "&::-webkit-scrollbar-thumb": {
+        "background-color": "#555",
+        borderRadius: "24px",
+      },
+    },
+  },
   colors: {
     brand: {
       100: "#f7fafc",
